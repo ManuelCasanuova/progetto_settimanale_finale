4a8567b6281e0221941e14629527cf70589cd332
ManuelCasanuova <manuelcasanuova@gmail.com>
2025-05-11 10:41:43 +0200

    Initial commit

---
+++ src/main/java/it/epicode/progetto_settimanale_finale/ProgettoSettimanaleFinaleApplication.java
@@ -0,0 +1,13 @@
+package it.epicode.progetto_settimanale_finale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettoSettimanaleFinaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgettoSettimanaleFinaleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/epicode/progetto_settimanale_finale/ProgettoSettimanaleFinaleApplicationTests.java
@@ -0,0 +1,13 @@
+package it.epicode.progetto_settimanale_finale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgettoSettimanaleFinaleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
